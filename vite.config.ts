c8bca479bb4ee62b959195f71e2da9bb3a31136c
akatsukinoyami <hirokod3@gmail.com>
2026-03-31 15:19:17 +0300

    add dev libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js';
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
+import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
 	plugins: [
+		mkcert(),
 		tailwindcss(),
 		sveltekit(),
-		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
+		devtoolsJson(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
 	]
-});
+})
+
