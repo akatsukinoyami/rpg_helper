39b3561947ee339f18a6fd18306ccd863a6124de
akatsukinoyami <hirokod3@gmail.com>
2026-04-13 14:34:30 +0300

    change code structure

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -5,3 +5,7 @@ export function keys<T extends object>(obj: T): (keyof T)[] {
 export function entries<T extends object>(obj: T): [keyof T, T[keyof T]][] {
 	return Object.entries(obj) as [keyof T, T[keyof T]][];
 }
+
+export * from '$lib/utils/md';
+export * from '$lib/utils/stats';
+export * from '$lib/utils/theme';
